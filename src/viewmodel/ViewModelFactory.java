b4e4c4d72bcc7d6e4bd73fe28c21fa4a31d8ea77
Uldis Alksnis <csmccs1@gmail.com>
2020-03-05 11:30:28 +0100

    ViewModelFactory

--- src/viewmodel/ViewModelFactory.java
+++ src/viewmodel/ViewModelFactory.java
@@ -12,4 +12,6 @@ public class ViewModelFactory
         this.detailsViewModel = new DetailsViewModel(model);
     }
 
+    public ThermometerViewModel getThermometerViewModel(){ return thermometerViewModel;}
+    public DetailsViewModel getDetailsViewModel(){return detailsViewModel;}
 }
